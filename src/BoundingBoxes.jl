27a3b278d2d97b26569ca6edc7826eade0c658a2
Gustavo Goretkin <gustavo.goretkin@gmail.com>
2019-12-09 13:53:32 -0500

    Oops, from weeks ago

--- src/BoundingBoxes.jl
+++ src/BoundingBoxes.jl
@@ -1,3 +1,114 @@
 module BoundingBoxes
 
+export BoundingBox, overlaps
+
+import GeometryTypes
+import GeometryTypes: overlaps
+import GeometryTypes.vertices
+import GeometryTypes.intersects
+import GeometryTypes.contains
+import GeometryTypes.widths
+import GeometryTypes.isinside
+
+using GeometryTypes: Point, Vec, HyperSphere, HyperRectangle
+
+import Base.convert
+using StaticArrays: SVector
+import NaNMath
+
+"Axis-aligned bounding box. Constructor doesn't check all(minimum .<= maximum)"
+struct BoundingBox{N,T} <: GeometryTypes.AbstractGeometry{N, T}
+  minimum::SVector{N,T}
+  maximum::SVector{N,T}
+end
+
+convert(::Type{HyperRectangle}, bb::BoundingBox) = HyperRectangle(Vec(minlimit(bb)), Vec(widths(bb)))
+function Base.convert(::Type{HyperRectangle{N, T}}, bb::BoundingBox) where {N, T}
+  HyperRectangle(Vec{N, T}(minlimit(bb)), Vec{N, T}(widths(bb)))
+end
+
+# splatting definition. TODO really should apply for anything whose size is not inferrable
+BoundingBox(mi::Vector, ma::Vector) = BoundingBox(promote(SVector(mi...), SVector(ma...))...)
+BoundingBox(mi, ma) = BoundingBox(promote(convert(SVector, mi), convert(SVector, ma))...)
+
+minlimit(b::BoundingBox) = b.minimum
+maxlimit(b::BoundingBox) = b.maximum
+dimlimits(b::BoundingBox{N}) where {N} = tuple(((b.minimum[i], b.maximum[i]) for i = 1:N)...)
+
+function contains(bb::BoundingBox{N, T}, p::Point{N, T}) where {N, T}
+  return all(maxlimit(bb) .>= p .>= minlimit(bb))
+end
+
+"""symmetric overlap relation"""
+function overlaps(b1::BoundingBox{N}, b2::BoundingBox{N}) where {N}
+  # can't exit early with broadcasting, but branching might be more expensive
+
+  #= # this is equivalent by DeMorgan's, except that for NaN behavior
+  disjoint_dimensions = all(maximum(b1) .< minimum(b2)) .| all(maximum(b2) .< minimum(b1))
+  return !(any(disjoint_dimensions))
+  =#
+  overlap_dimensions = (maxlimit(b1) .≥ minlimit(b2)) .& (maxlimit(b2) .≥ minlimit(b1))
+  return all(overlap_dimensions)
+end
+
+"make bounding box from array of points"
+function BoundingBox(points::AbstractVector) #{PT}) where {T, PT<:AbstractArray{T,1}}
+  # errors if length(points) == 0
+  #all(length.(points) .== length(points[1]))  # Hope it is elided for static arrays. Doesn't work on iterators
+  #mi, ma = extrema(points) # need to reduce over min., max.
+  if length(points) == 0
+    N = length(eltype(points))
+    T = eltype(eltype(points))
+    nan = nan(T)
+    BoundingBox(fill(nan, N), fill(nan, N))
+  end
+  mi = reduce((x,y)->min.(x,y), points) # TODO sad for performance
+  ma = reduce((x,y)->max.(x,y), points)
+
+  # works on empty collections
+  #mi = reduce((x,y)->NaNMath.min.(x,y), nanvector, points) # TODO sad for performance
+  #ma = reduce((x,y)->NaNMath.max.(x,y), nanvector, points)
+
+  return BoundingBox(mi, ma)
+end
+
+import Base.union
+function union(b1::BoundingBox{N,T1}, b2::BoundingBox{N,T2}) where {N, T1, T2}
+  # NaN represents empty.
+  BoundingBox( NaNMath.min.(minlimit(b1), minlimit(b2)), NaNMath.max.(maxlimit(b1), maxlimit(b2)) )
+end
+
+BoundingBox(point::Point) = BoundingBox(point, point)
+BoundingBox(s::HyperSphere) = BoundingBox(s.center .- s.r, s.center .+ s.r)
+# TODO use a lazy (geometric)-union type from another package
+#BoundingBox(u::Union{UnionOfConvex, UnionOfGeometry}) = reduce(union, map(BoundingBox, u.pieces))
+
+"""is ball inside box"""
+@inline function contains(box::BoundingBox{N, T}, ball::HyperSphere{N, T}) where {N, T}
+  r = ball.r
+  c = ball.center
+  for i = 1:N
+    if !(
+        box.maximum[i] > c[i] + r   &&
+        box.minimum[i] < c[i] - r
+        )
+      return false
+    end
+  end
+  return true
+end
+
+
+@inline function center(box::BoundingBox)
+  widths(box) / 2 + box.minimum
+end
+
+@inline function widths(box::BoundingBox)
+  box.maximum - box.minimum
+end
+
+
+@inline vertices(box::BoundingBox) = vertices(HyperRectangle(minlimit(box)..., widths(box)...))
+
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,42 @@
 using BoundingBoxes
 using Test
+
+b_ref = BoundingBox([1.0, 1.0], [2.0, 2.0])
+b_inside = BoundingBox([1.2, 1.2], [1.8, 1.8])
+b_overlap1 = BoundingBox([1.8, 1.8], [2.2, 2.2])
+b_overlap2 = BoundingBox([0.8, 0.8], [1.2, 1.2])
+b_overlap3 = BoundingBox([1.8, 0.8], [2.2, 1.2])
+b_overlap4 = BoundingBox([0.8, 1.8], [1.2, 2.2])
+b_outside_all1 = BoundingBox([3.0, 3.0], [4.0, 4.0])
+b_outside_all2 = BoundingBox([-4.0, -4.0], [-3.0, -3.0])
+b_outside_all3 = BoundingBox([3.0, -4.0], [4.0, -3.0])
+b_outside_all4 = BoundingBox([-4.0, 3.0], [-3.0, 4.0])
+b_outside_partial1 = BoundingBox([1.1, 3.0], [2.1, 4.0])
+b_outside_partial2 = BoundingBox([3.0, 1.1], [4.0, 2.1])
+b_outside_partial3 = BoundingBox([1.2, 3.0], [1.8, 4.0])
+b_outside_partial4 = BoundingBox([3.0, 1.2], [4.0, 1.8])
+b_outside_inf = BoundingBox([3.0, 3.0], [Inf, Inf])
+b_overlap_inf = BoundingBox([1.8, 1.8], [Inf, Inf])
+b_overlap_inf2 = BoundingBox([-Inf, -Inf], [Inf, Inf])
+b_empty = BoundingBox([NaN, NaN], [NaN, NaN])
+
+b_true = [b_inside, b_overlap1, b_overlap2, b_overlap3, b_overlap4, b_overlap_inf, b_overlap_inf2]
+b_false = [b_outside_all1, b_outside_all2, b_outside_all3, b_outside_all4, b_outside_partial1,
+  b_outside_partial2, b_outside_partial3, b_outside_partial4, b_outside_inf, b_empty]
+
+function overlap_sym(a, b, answer)
+  @test overlaps(a, b)==answer
+  @test overlaps(b, a)==answer
+end
+
+for b in b_true
+  overlap_sym(b_ref, b, true)
+end
+
+for b in b_false
+  overlap_sym(b_ref, b, false)
+end
+
+for b in vcat(b_true, b_false)
+  overlap_sym(b_empty, b, false)
+end
